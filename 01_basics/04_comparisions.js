c6509b62b965feccb8ee4e197a1f7ac2c040cd94
Sampada Kaushik <143282629+sampada109@users.noreply.github.com>
2024-10-27 08:45:50 +0000

    comparision

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,32 @@
+// there can many type of comparisions
+
+// number to number comparision
+// console.log(2 > 1);
+// console.log(4 >= 9);
+// console.log(8 != 0);
+
+
+// string and number comparision
+// console.log("2" > 3);      //here conversion happens
+// console.log(1 < "2");      // string is converted to number for comparision
+
+
+// null and undefined
+// here the output is unpredictable
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+// one thing to note is that == and >,< are both different operaters and have different approch for comparision
+
+
+// '===' these are use for strict comparision
+// means it check the two values and also checks its data type
+console.log("2" == 2);
+console.log("2" === 2);
+
+ 
